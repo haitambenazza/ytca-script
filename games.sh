6fa19789078ac4202288f053ba756833a7cb5585
hbenazza <hbenazza@c4r10p6.1337.ma>
2025-02-23 10:56:32 +0000

    games

---
+++ games.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+flatpak run com.google.Chrome https://www.atlantisbahamas.com/memory-game/#memory-game
+flatpak run com.google.Chrome https://gotm.doteye.online/#February
--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /home/ytca2025/Desktop
+cd $HOME/Desktop
 mkdir $1
 cd $1
 touch introduce_yourself.py
@@ -9,8 +9,10 @@ touch my_awesome_counter.py
 touch quadratic_formula.py
 touch biggest_number.py
 
-chmod +rwx introduce_yourself.py
-chmod +rwx odd_even.py
-chmod +rwx my_awesome_counter.py
-chmod +rwx quadratic_formula.py
-chmod +rwx biggest_number.py
+chmod u+rwx introduce_yourself.py
+chmod u+rwx odd_even.py
+chmod u+rwx my_awesome_counter.py
+chmod u+rwx quadratic_formula.py
+chmod u+rwx biggest_number.py
+
+flatpak run com.visualstudio.code .
